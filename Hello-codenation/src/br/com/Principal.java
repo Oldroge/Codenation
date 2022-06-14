c23fc13b476f19f9ab0c3e55a3c1e5e03a878ae9
OldRoge <rogeriomrjr@icloud.com>
2022-06-14 12:07:25 -0300

    first commit

---
+++ Hello-codenation/src/br/com/Principal.java
@@ -0,0 +1,13 @@
+// Como toda aplicação Java precisa obrigatoriamente estar dentro de um pacote, 
+package br.com;
+
+public class Principal {
+//    Este é o método main, que seria para o dar o ponto de partida para a aplicação
+//    Toda aplicação precisa ser inicializada de algum lugar,
+//    e através deste método Main, é onde a aplicação Java se inicia
+    public static void main(String[] args) {
+//      Esta seria a maneira de imprimir alguma mensagem no console,
+//      fazendo uma analogia, seria como o 'console.log' do JavaScript
+        System.out.println("Hello Codenation");
+    }
+}
